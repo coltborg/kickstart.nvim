e05783b3e26acce5897546e0e6d89963c9d5dee4
coltborg <borg@hey.com>
2024-03-04 11:20:44 -0600

    Add LazyGit plugin

--- init.lua
+++ init.lua
@@ -274,6 +274,24 @@ require('lazy').setup({
     }
   },
 
+  -- https://github.com/kdheepak/lazygit.nvim
+  {
+    {
+      "kdheepak/lazygit.nvim",
+      cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+      },
+      -- optional for floating window border decoration
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+      },
+    },
+  },
+
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
